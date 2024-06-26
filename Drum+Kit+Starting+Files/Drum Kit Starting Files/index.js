5673f81c277b9919976c775392c271e561c61fec
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-26 18:52:26 +0200

    Higher Order Functions and Passing Functions as Arguments

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -35,12 +35,48 @@ function calculator(num1, num2, operator) {
 
 
 
-let numberOfDruwBotton = document.querySelectorAll(".drum").length;
+var numberOfDruwBotton = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDruwBotton; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        // this.style.color = "white";
+        var buttonInnerHTML = this.innerHTML;
+
+
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
     });
 
 }
